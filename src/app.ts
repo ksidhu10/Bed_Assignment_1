54b7675c97364df46b1c2c1ca772bb31e8d6b91a
rrc_w2024_kiran <ksidhu10@academic.rrc.ca>
2025-01-19 19:00:33 -0600

    feat: add health check endpoint with status, uptime, and version

--- src/app.ts
+++ src/app.ts
@@ -25,5 +25,23 @@ app.get('/', (req, res) => {
 app.get("/tasks", (req, res) => {
 	res.send("Retrieve tasks");
 });
+/**
+  * @openapi
+  * /api/v1/health:
+  *  get:
+  *      summary: Quick healthCheck
+  *      tags: [Health]
+  *      responses:
+  *          200:
+  *              description: JSON response 
+*/
+app.get("/api/v1/health", (req, res) => {
+    res.json({
+      status: "OK",
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString(),
+      version: "1.0.0",
+    });
+  });
 
 export default app;
