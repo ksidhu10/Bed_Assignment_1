5339a9f22399af6ecf45972d44e72f03c7ca1040
rrc_w2024_kiran <ksidhu10@academic.rrc.ca>
2025-01-19 18:29:04 -0600

    setup: install and configure Jest with ts-jest and Supertest for automated API testing

--- config/swagger.ts
+++ config/swagger.ts
@@ -3,21 +3,21 @@ import swaggerJsDoc, { Options } from "swagger-jsdoc";
 import { Express } from "express";
 
 const swaggerOptions: Options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API Documentation",
-      version: "1.0.0",
-    },
-  },
-  apis: ["./src/routes/*.ts"], 
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "API Documentation",
+			version: "1.0.0",
+		},
+	},
+	apis: ["./src/app.ts"], // Path to the API docs when routes are in app.ts
 };
 
-// Define swaggerDocs as the correct type from swagger-jsdoc
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const swaggerDocs: any = swaggerJsDoc(swaggerOptions);
 
 const setupSwagger = (app: Express): void => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 };
 
 export default setupSwagger;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
 import express, { Express} from 'express';
+import setupSwagger from "../config/swagger";
 
+// Initialize Express application
 const app: Express = express();
+setupSwagger(app);
 
 // Define a simple route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+
+/**
+ * @openapi
+ * /tasks:
+ *   get:
+ *     summary: Retrieve a list of tasks
+ *     tags: [Tasks]
+ *     responses:
+ *       200:
+ *         description: A list of tasks
+ */
+
+app.get("/tasks", (req, res) => {
+	res.send("Retrieve tasks");
 });
 
 export default app;
